7da2fb378e54f359cd3aa01ed48cd8b6df99dbc5
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2023-09-18 00:00:13 +0900

    타겟넘버 / LV2

---
+++ Seongmin/코딩테스트 고득점 Kit/DFS_BFS/TargetNumber.java
@@ -0,0 +1,26 @@
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+
+  
+public class TargetNumber {
+  //재귀함수
+  int answer;
+  public int solution(int[] numbers, int target) {
+    answer = 0;
+    dfs(0, 0, numbers, target);
+    return answer;
+  }
+  void dfs(int n, int sum, int[] numbers, int target) {
+    if(n == numbers.length) {
+      if(sum == target) 
+        answer++;
+      return;
+    }
+    dfs(n + 1, sum + numbers[n], numbers, target);
+    dfs(n + 1, sum - numbers[n], numbers, target);
+  }
+}
