9730fdb2f286a47620eede534993a45247a768b2
홍성민 <79087697+hongseungmn@users.noreply.github.com>
2023-10-04 22:05:48 +0900

    전화번호 목록 /LV 2

---
+++ Seongmin/코딩테스트 고득점 Kit/해시/PhoneNumberList.java
@@ -0,0 +1,20 @@
+/*
+ * 전화번호 목록
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42577
+ */
+//홍성민
+import java.util.Arrays;
+
+public class PhoneNumberList {
+  public boolean solution(String[] phone_book) {
+    boolean answer = true;
+    Arrays.sort(phone_book);
+    for(int i=0;i<phone_book.length-1;i++) {
+        if(phone_book[i+1].startsWith(phone_book[i])) {
+          return false;
+        }
+      
+    }
+    return answer;
+}
+}
